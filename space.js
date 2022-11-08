f62936bf1b16d605d4136b85422f042d440a9f2a
Arianna Bunn <ajbunny12@gmail.com>
2022-11-07 23:30:18 -0500

    was able to finally get the battle round to work

--- space.js
+++ space.js
@@ -14,31 +14,29 @@ class HumanShip extends Ship {
     super(hull, firepower, accuracy);
   }
   attack(meanAlien) {
-    if (this.hull > 0) {
-      if (Math.random() < this.accuracy) {
-        meanAlien.hull -= this.firepower;
+   
+    // if (this.hull > 0) {
+      if (this.checkAlienAccuracy(this.accuracy)) {
+         meanAlien.hull -= this.firepower;
         console.log(`BunnyShip attacked the Alien ship and they have ${alienArr[0].hull} left`);
-      } else {
-        console.log("Earthship attack missed!");
-      }
-    } else {
-      for (let i = 0; i < alienArr[0].length; i++) {
-        if (alienArr[i].hull < 0) {
-          console.log("Continue attack or Retreat");
-        }
-      }
+      } //else {
+    //     console.log("Earthship attack missed!");
+    //   }
+     else {
+      console.log('We missed! Load next missile!')
     }
+    return meanAlien.hull
   }
-}
-const hit = true
-function checkBunnyAccuarcy(nicehuman_accuracy) {
-    if (hit) {
-        return true
-    }
-    else
-    return false 
-}
 
+ checkAlienAccuracy(accuracy) {
+    let check = Math.random()
+    if (check < accuracy) {
+        return true
+    } else {
+    return false 
+    }
+}
+}   
 //#endregion
 
 //#region  ALIEN CLASS
@@ -50,35 +48,36 @@ class AlienShip extends Ship {
     this.accuracy = accuracy; //between .6-.8
   }
   attack(niceHuman) {
-    if(checkBunnyAccuarcy(niceHuman.accuracy))
-       
-    if (this.hull > 0) {
-      if (Math.random() > this.accuracy)
-        BunnyShip.hull = BunnyShip.hull -= this.firepower;
+    if (this.checkBunnyAccuracy(this.accuracy)) {   
+        niceHuman.hull -= this.firepower;
       console.log(
         `The Alien ship atttacked the Bunnyship. The earth ship has this ${BunnyShip.hull} much left!`
       );
     } else {
       console.log("You hit the Human Ship!");
     }
+    return niceHuman.hull
   }
-// Bunny will attack first{
-//     one bunny ship && one alien ship (nicehuman, meanalien)
-//         if(check accuracy (bunny.accuracy))  
-//              if true === alien hull -= nicehuman.firepower
-       
-//             if false = console.log 'we missed'
-//          return alien hull value   
+
+  
+ checkBunnyAccuracy(accuracy) {
+    let check = Math.random()
+   if (check < accuracy) {
+       return true
+   } else {
+   return false 
+   }
+ }
+    
+}
+//  function checkAlienAccuracy(meanAlien_accuracy) {
+//     if (hit) {
+//         return true
+//     }
+//     else
+//     return false 
 // }
 
-}
-function checkAlienAccuarcy(meanAlien_accuracy) {
-    if (hit) {
-        return true
-    }
-    else
-    return false 
-}
 //#endregion
 
 //#region Random Num function
@@ -146,118 +145,79 @@ alienArr.push(
 
 //#region  Create battle btwn Bunny's ship and alien ship using booleans
 const battleRound = (aliens, earthShip) => {
+    
   let winnerOfRound = true;
-  while (true) {
-      if (earthShip.hull > 0 && aliens[0].hull >= 1) {
+  while (winnerOfRound) {
+    if (earthShip.hull > 0 && aliens[0].hull >= 1) {
         console.log("Attack the Aliens!!");
-        earthShip.attack(aliens);
+        earthShip.attack(aliens[0]);
       }
-      if (aliens[0].hull > 1 && earthShip.hull > 0) {
+      if (aliens[0].hull > 0 && earthShip.hull > 0) {
         console.log("Oh no, They're targeting BunnyShip");
         aliens[0].attack(earthShip);
       }
-      if (earthShip.hull > 1 && aliens[0].hull === 0) {
+      if (earthShip.hull > 0 && aliens[0].hull <= 0) {
         console.log("We are defeating the Aliens!");
-        earthShip.attack(aliens[i]);
-      } else {
+        winnerOfRound = false;
+        console.log('YOU SAVED THE PLANET')
+        
+      } 
+      
+      else {
         console.log("You Lose. Game Over.");
-        return;
+        winnerOfRound =  false;
       }
-    
-  }
+}
+
+
 };
 
-
- 
 battleRound(alienArr, BunnyShip);
 
+
 //#endregion
+
+
+// 
+
 
 //#region GAME OBJECT ///////
 let game = {
-  checkEarthWon: function () //checks life status of alien ife
-  {
-    if (meanAlien.hull < 1) {
-      console.log("Aliens have been destroyed!!");
-    }
+  checkEarthWon: () => {
+        if (aliens[0].hull < 1) {
+            console.log("Aliens have been destroyed!!");
+        }
+    },
+
+  checkAlienWon: () => {
+      if (BunnyShip.hull < 1) { 
+
+      }
+      console.log("You Lost, Game Over!");
+  },
+  checkALienHealth: ()  =>  // alien's hull left
+   {return alienArr[0].hull;
   },
 
-  checkAlienWon: function () {
-    //checks status of Bunny's life
-    if (BunnyShip.hull < 1) {
-      return true;
-    }
-  },
-
-  checkALienHealth: function () {
-    // alien's hull left
-    return alienArr[0].hull;
-  },
-
-  checkBunnyHealth: function () {
-    // Bunny's hull left
+  checkBunnyHealth:() =>   // Bunny's hull left
+    {
     return BunnyShip.hull;
   },
-};
+  
 
-// startBattle() {
-//     while((this.checkPlayerHP()) && (this.checkAlienHP())){
-//         USS_Terminator.attack(alienFleet[0]) && alienFleet[0].attack(USS_Terminator)
-//         console.log("USS_Terminator locks on to ALien Ship")
-//         this.humanShip.attack(this.enemyShips[0])
-//         console.log(`Alien has ${this.enemyShips[0].hull} left`)
-//         console.log("Alien takes aim at USS_Terminator")
-//         this.enemyShips[0].attack(this.humanShip)
-//         console.log(`USS_Terminator has ${this.humanShip.hull} left`)
-//     }
-//     if(this.checkAlienHP() == false){
-//         console.log("USS_Terminator has defeated an alien ship")
-//     }else if(this.checkPlayerHP() == false){
-//         console.log("Aliens have destroyed the USS_Terminator")
-//     }
-// }
+  pickTarget() {                                                                              //function to return last index in alien array
+    return this.alienArr[this.alienArr.length - 1]
+},
+
+makeAliens() {
+    let alienArr = []
+    for (let i = 6; i > 0; i--) {
+        alienArr.push(new AlienShip(`Alien Ship #${i}`))  
+    }
+    return alienArr
+}
+};
+// console.log(game);
 
 //#endregion
 
-/*
-Bunnyship
-AlienShip 
-they will BANG BANG each other -- this will be a function 
-Attack functin will pull everything in it
-    check accuracy = (bunny.accuracy) => {
-        if hit (return true)
-        else return false 
-    } 
-    Bunny will attack first{
-    one bunny ship && one alien ship (nicehuman, meanalien)
-        if(check accuracy (bunny.accuracy))  
-             if true === alien hull -= nicehuman.firepower
-       
-            if false = console.log 'we missed'
-         return alien hull value   
-}
- 
-Alien will attack 
-alien ship && bunny ship
-
-    Alien will attack second{
-    one bunny ship && one alien ship (nicehuman, meanalien)
-        if(check accuracy (.accuracy))  
-             if true === bunny hull -= meanalien.firepower
-       
-            if false = console.log 'we missed'
-         return bunny's hull value   
-}
-    
-let enemy's hull status = Bunny.attack();
-One will survive(enemy's hull status) = 
-    if(enemy's hull status =< 0)
-        return 
-
-    
-
-return value of hull points
-
-Play or Retreat 
-
-*/
